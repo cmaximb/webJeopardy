626a092113c9618969742dd72c58aeaba4d16bd2
cmaximb <138892841+cmaximb@users.noreply.github.com>
2023-10-14 18:49:01 +0100

    Add files via upload

---
+++ webjeopardy/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+import filmtrumps.jeopardy as jeopardy
+import random
+
+app = Flask(__name__)
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    question = "tests"
+
+    times = 1
+    answerscategories = []
+    for j in range(5):
+        n = random.randint(0, 20000)
+        category = n
+        answerscategories.append(jeopardy.ask_question(category))
+        
+    return render_template('hello.html', name=name, answers=answerscategories)
+
---
+++ webjeopardy/jeopardy.py
@@ -0,0 +1,20 @@
+import html2text
+import requests
+
+parser = html2text.HTML2Text()
+parser.emphasis_mark = "http://avon.cs.nott.ac.uk/~psycb9/dbiexample/Homepage.html"
+
+def ask_question(jeopc):
+    categories = []
+    responses = requests.get("http://jservice.io/api/clues", params = {"category": jeopc})
+    for m in range(5):
+        try: 
+            print(responses.json()[m]["question"])
+            response = responses.json()[m]
+            parser.handle(response["answer"])
+            parser.handle(response["question"])
+        except:
+            response = []
+        categories.append(response)
+
+    return categories
